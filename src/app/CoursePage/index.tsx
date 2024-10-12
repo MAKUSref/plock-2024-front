d38a46c5a7bda4ec16a09585ccae9adaf06ea4c4
kasia251011 <katarzyna.jankowska000728@gmail.com>
2024-10-13 01:12:23 +0200

    Curse tabs for admin

--- src/app/CoursePage/index.tsx
+++ src/app/CoursePage/index.tsx
@@ -1,18 +1,22 @@
+import About from "@/components/course/About";
 import CourseDetailsCard from "@/components/course/CourseDetailsCard";
+import CourseTabs from "@/components/course/CourseTabs";
 import { useGetCourseQuery } from "@/redux/api/courseApi";
-import { UserOutlined } from "@ant-design/icons";
-import { Avatar } from "antd";
+import { useUserRole } from "@/redux/selectors";
 import { useParams } from "react-router-dom";
 
 const CoursePage = () => {
   const { id } = useParams<{ id: string }>();
   const { data: course } = useGetCourseQuery(id || "");
+  const userRole = useUserRole();
+
+  console.log(userRole);
 
   return (
     <div>
       <div
         style={{ backgroundImage: `url(${course?.coverImgSrc})` }}
-        className="relative flex items-end h-[50vh] w-full bg-no-repeat bg-center bg-cover"
+        className="relative flex items-end h-[70vh] w-full bg-no-repeat bg-center bg-cover"
       >
         <div className="absolute bg-black bg-opacity-60 w-full h-full top-0 left-0 bottom-0 right-0"></div>
         <div className="container relative pb-8">
@@ -25,27 +29,16 @@ const CoursePage = () => {
       </div>
       <div className="container">
         <div className="grid grid-cols-12 gap-4">
-          <div className="col-span-8 mt-20">
-            <h3 className="mb-6">O czym jest kurs?</h3>
-            <p>
-              {course?.description} Many desktop publishing packages and web
-              page editors now use Lorem Ipsum as their default model text, and
-              a search for 'lorem ipsum' will uncover many web sites still in
-              their infancy. Various versions have evolved over the years,
-              sometimes by accident, sometimes on purpose (injected humour and
-              the like).
-            </p>
-            <h3 className="mt-20 mb-10">Prelegenci</h3>
-            <div className="flex gap-20 text-center">
-              {course?.lecturers.map((speaker, index) => (
-                <div key={index} className="flex flex-col gap-3">
-                  <Avatar size={200} icon={<UserOutlined />} />
-                  <p className="font-semibold">{speaker.name}</p>
-                  <p>{speaker.description}</p>
-                </div>
-              ))}
+          {userRole === "admin" || userRole === "lecturer" ? (
+            <div className="col-span-8 mt-20">
+              <CourseTabs />
             </div>
-          </div>
+          ) : (
+            <div className="col-span-8 mt-20">
+              <About />
+            </div>
+          )}
+
           <div className="col-span-3 col-start-10">
             <CourseDetailsCard />
           </div>
---
+++ src/components/course/Files/index.tsx
@@ -0,0 +1,9 @@
+const Files = () => {
+  return (
+    <div>
+      <h3>Files</h3>
+    </div>
+  );
+};
+
+export default Files;
