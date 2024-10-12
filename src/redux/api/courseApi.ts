420e6fcb1f10215601210a15c2bb3cd3ff37c837
MAKUSref <tomaszmakowski002731@gmail.com>
2024-10-12 23:48:15 +0200

    fix tag filter

--- src/redux/api/courseApi.ts
+++ src/redux/api/courseApi.ts
@@ -3,18 +3,20 @@ import { Course } from "@/types/course";
 
 export const courseApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
-    getCourses: builder.query<
-      Course[],
-      { searchText?: string; searchTag?: string }
-    >({
+    getCourses: builder.query<Course[], { searchText?: string; searchTag?: string }>({
       query: ({ searchTag, searchText } = {}) => {
+        const queryBuilder = [];
+        if (searchTag) {
+          queryBuilder.push(`tags=${searchTag}`);
+        }
+        if (searchText) {
+          queryBuilder.push(`search=${searchText}`);
+        }
+        const queryParams = queryBuilder.join("&");
+
         return {
-          url: `/course`,
-          params: {
-            tags: searchTag,
-            search: searchText,
-          },
-        };
+          url: `/course${queryParams !== "" ? "?" + queryParams : ""}`,
+        }
       },
     }),
     getCourse: builder.query<Course, string>({
