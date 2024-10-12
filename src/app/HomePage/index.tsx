6ba666bea9c4a77ad0eaa0ea018c7700e8a91d69
MAKUSref <tomaszmakowski002731@gmail.com>
2024-10-13 01:31:47 +0200

    Create template for home page users panel

--- src/app/HomePage/index.tsx
+++ src/app/HomePage/index.tsx
@@ -1,23 +1,43 @@
 import CourseList from "@/components/home/CourseList";
 import HomeIntro from "@/components/home/Intro";
-import { useAuth } from "@/redux/selectors";
+import { useAuth, useUserRole } from "@/redux/selectors";
 
 const HomePage = () => {
   const isAuth = useAuth();
+  const userRole = useUserRole();
 
-  console.log(isAuth);
+  console.log(userRole);
+
+  if (!isAuth) {
+    return (
+      <div>
+        <div className="absolute top-0 left-0 right-0 -z-10">
+          <img src="/img/home-intro-top.svg" alt="..." className="w-full" />
+        </div>
+        <HomeIntro />
+        <div className="container">
+          <CourseList />
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div>
       <div className="absolute top-0 left-0 right-0 -z-10">
         <img src="/img/home-intro-top.svg" alt="..." className="w-full" />
       </div>
-      {isAuth ? (
-        <h1 className="text-2xl font-bold">Witaj w panelu użytkownika</h1>
-      ) : (
+      {userRole?.toLowerCase() === "admin" && (
         <HomeIntro />
       )}
 
+      {userRole?.toLowerCase() === "lecturer" && (
+        <HomeIntro />
+      )}
+
+      {userRole?.toLowerCase() === "user" && (
+        <HomeIntro />
+      )}
       <div className="container">
         <CourseList />
       </div>
