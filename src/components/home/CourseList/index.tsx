2b1f6c295916d92592a488aad2386460bce72add
MAKUSref <tomaszmakowski002731@gmail.com>
2024-10-12 23:17:58 +0200

    create filter by tags

---
+++ src/components/home/CourseList/index.tsx
@@ -0,0 +1,84 @@
+import SearchInput from "@/components/common/SearchInput";
+import CourseCard from "@/components/course/CourseCard";
+import { useGetCoursesQuery } from "@/redux/api/courseApi";
+import { Card, Skeleton, Tag } from "antd";
+import { useState } from "react";
+
+interface CourseTag {
+  name: string;
+  color: string;
+  active: string;
+}
+
+const TAGS: CourseTag[] = [
+  {
+    name: "Marketing",
+    color: "purple",
+    active: "purple-inverse"
+  },
+  {
+    name: "AI",
+    color: "blue",
+    active: "blue-inverse"
+  },
+  {
+    name: "ZdrowiePsychiczne",
+    color: "magenta",
+    active: "magenta-inverse"
+  },
+  {
+    name: "Excel",
+    color: "green",
+    active: "green-inverse"
+  }
+];
+
+const CourseList = () => {
+  const [searchText] = useState("");
+  const [searchTag, setSearchTag] = useState("");
+  const { data: courses, isLoading } = useGetCoursesQuery({ searchTag, searchText });
+
+  const handleSearchTag = (tag: string) => {
+    if (searchTag === tag) {
+      setSearchTag("");
+      return;
+    }
+    setSearchTag(tag);
+  }
+
+  return (
+    <>
+      <div className="flex flex-col items-center">
+        <div className="max-w-[500px] w-full py-3">
+          <SearchInput />
+        </div>
+        <div className="flex gap-1 py-3 flex-wrap">
+          {TAGS.map((tag, i) => (
+            <Tag
+              key={i}
+              bordered={false}
+              color={tag.name === searchTag ? tag.active : tag.color}
+              className="cursor-pointer px-[10px] text-base"
+              onClick={() => handleSearchTag(tag.name)}
+            >
+              #{tag.name}
+            </Tag>
+          ))}
+        </div>
+      </div>
+      {isLoading && (
+        <Card loading cover={<Skeleton.Image active />}>
+          <Card.Meta title="Nazwa szkolenia" description="Opis szkolenia" />
+        </Card>
+      )}
+
+      <div className="mt-20 grid grid-cols-3 gap-4">
+        {courses?.map((course, index) => (
+          <CourseCard key={index} course={course} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default CourseList;
--- src/components/home/Intro/index.tsx
+++ src/components/home/Intro/index.tsx
@@ -1,56 +1,13 @@
-import SearchInput from "@/components/common/SearchInput";
-import { Tag } from "antd";
-
 const Intro = () => {
   return (
     <div className="">
-      <div>
-        <img
-          src="/img/home-intro-top.svg"
-          alt="..."
-          className="absolute z-0 w-full top-0"
-        />
-      </div>
       <div className="container">
-        <div className="h-[75vh] flex flex-col justify-end items-center">
+        <div className="h-[65vh] flex flex-col justify-end items-center">
           <h1>Baza szkoleń PPPT</h1>
           <p className="font-normal mb-12 text-slate-600">
             Zbiór wszystkich szkoleń organizowanych przez Płocki Park
             Przemysłowo-Technologiczny.
           </p>
-          <div className="max-w-[500px] w-full py-3">
-            <SearchInput />
-          </div>
-          <div className="flex gap-1 py-3 flex-wrap">
-            <Tag
-              bordered={false}
-              color="purple"
-              className="cursor-pointer font-semibold px-[10px]"
-            >
-              #biznes
-            </Tag>
-            <Tag
-              bordered={false}
-              color="blue"
-              className="cursor-pointer font-semibold px-[10px]"
-            >
-              #AI
-            </Tag>
-            <Tag
-              bordered={false}
-              color="magenta"
-              className="cursor-pointer font-semibold px-[10px]"
-            >
-              #ZdrowiePsychiczne
-            </Tag>
-            <Tag
-              bordered={false}
-              color="green"
-              className="cursor-pointer font-semibold px-[10px]"
-            >
-              #Excel
-            </Tag>
-          </div>
         </div>
       </div>
     </div>
